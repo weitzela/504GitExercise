cf022725392ef34061e2802f156c8310860ee344
Anne Weitzel <anne.weitzel@gmail.com>
2022-08-24 10:07:17 -0400

    amke more detailed function names

--- code.py
+++ code.py
@@ -1,4 +1,15 @@
-def function1(a):
+def count_occurances(a):
+    '''
+    Counts the number of times each letter occurs in the input string. Returns
+    a dictionary with names of each nucleotide with the number of off occurances
+    for each nucleotide.
+
+        Parameters:
+                a (str): a string of nucleotides
+
+        Returns:
+                b (dict): dictionary of counts. keys are nucleotides.
+    '''
     b = dict()
     for c in a:
         if c not in b:
@@ -7,10 +18,12 @@ def function1(a):
             b[c] += 1
     return b
 
-def function2(a):
+def count_to_prop(a):
     print('freqs')
     total = float(sum([a[b] for b in a.keys()]))
     for b in a.keys():
         print(b + ':' + str(a[b]/total))
 
-function2(function1('ATCTGACGCGCGCCGC'))
+counts = count_occurances('ATCTGACGCGCGCCGC')
+prop = count_to_prop(counts)
+prop
